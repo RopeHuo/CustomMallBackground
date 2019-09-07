ee2d49a38ec84de3ee54b3734642523f986f069c
chenchangping <chenchangping@foxmail.com>
2019-09-07 15:49:32 +0800

    初始化PHP框架

---
+++ tp/app/api/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\api\controller;
+
+use app\BaseController;
+
+class Index extends BaseController
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+}
