b6ce068edf40d6feea64760ab3fb92f61da8ab12
huoqingchi <123203557@qq.com>
2019-09-17 14:29:48 +0800

    实现点击切换

--- react_project/src/pages/index/Index.js
+++ react_project/src/pages/index/Index.js
@@ -2,71 +2,102 @@
 import "./style.less";
 import React, { Component } from 'react';
 import { connect } from 'dva';
+import { push } from 'react-router-redux';
 
-import BsicData from '../basicData/BasicData';
-
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
 class Index extends Component {
    constructor(props) {
       super();
-
+      this.state = {
+         collapsed: false,
+      };
       //拉取默认数据
       // props.dispatch({ "type": "sidebar/get_list" });            
    }
 
+   onCollapse = collapsed => {
+      this.setState({ collapsed });
+   };
+   switching_components = e =>{      
+      this.props.dispatch({ "type":"navigation/modify_current_page","n":e.key})
+      this.props.dispatch(push(e.item.props.path))
+   }
    render() {
-      const { history } = this.props;//路由跳转使用
       return (
          <div>
-            <Layout style={{ height: '100vh' }}>
-               <Header className="header">
-                  <div className="logo" />
-                  <Menu
-                     theme="dark"
-                     mode="horizontal"
-                     defaultSelectedKeys={['2']}
-                     style={{ lineHeight: '64px' }}
-                  >
-                     <Menu.Item key="1"
-                        onClick={() => history.push('/basic_data')}
-                     >nav 1</Menu.Item>
+            <Header className="header"
+               style={{
+                  width: '100vw',
+                  position: 'fixed',
+                  top: 0,
+               }}
+            >
+               <div className="logo">多多美测试版
+               <sup style={{fontSize: "12px"}}>2.00dev</sup>
+               </div>
+               <Menu
+                  theme="dark"
+                  mode="horizontal" defaultSelectedKeys={[this.props.current_page]}
+                  style={{ lineHeight: '64px' }}
+               >
+                  <Menu.Item 
+                  onClick={this.switching_components} 
+                  path="/" key="1">后台首页</Menu.Item>
+                  
+                  <Menu.Item 
+                  onClick={this.switching_components} 
+                     path="/BasicData" key="2">基础数据</Menu.Item>
+                  
+                  <Menu.Item 
+                  onClick={this.switching_components} 
+                     path="/PurchaseSale" key="3">进销存管理</Menu.Item>
+                  
+                  <Menu.Item 
+                  onClick={this.switching_components} 
+                     path="/SystemManagement" key="11">系统管理</Menu.Item>
+               </Menu>
+            </Header>
+            <Layout>
+               <Sider
+                  style={{
+                     overflow: 'auto',
+                     height: '100vh',
+                     position: 'fixed',
+                     left: 0,
+                     top: 64
+                  }}
+                  collapsible
+                  collapsed={this.state.collapsed}
+                  onCollapse={this.onCollapse}
+               >
+                  <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                     <SubMenu
+                        key="sub1"
+                        title={
+                           <span>
+                              <Icon type="user" />
+                              <span>User</span>
+                           </span>
+                        }
+                     >
+                        <Menu.Item key="3">Tom</Menu.Item>
+                        <Menu.Item key="4">Bill</Menu.Item>
+                        <Menu.Item key="5">Alex</Menu.Item>
+                     </SubMenu>
                   </Menu>
-               </Header>
+               </Sider>
                <Layout>
-                  <Sider width={200} style={{ background: '#fff' }}>
-                     <Menu
-                        mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        style={{ height: '100%', borderRight: 0 }}
-                     >
-                        <SubMenu
-                           key="sub1"
-                           title={<span><Icon type="user" />subnav 1</span>}
-                        >
-                           <Menu.Item key="1">option2</Menu.Item>
-                           <Menu.Item key="2">option2</Menu.Item>
-                           <Menu.Item key="3">option3</Menu.Item>
-                           <Menu.Item key="4">option4</Menu.Item>
-                        </SubMenu>
-                     </Menu>
-                  </Sider>
-                  <Layout style={{ padding: '0 0 0 24px' }}>
-                     <Content
-                        style={{
-                           background: '#fff',
-                           padding: 24,
-                           margin: 0,
-                           minHeight: 280,
-                        }}
-                     >
-                        {this.props.id}
-                        <BsicData></BsicData>
-                     </Content>
-                  </Layout>
+                  <Content style={{ margin: '24px 0 0 16px', overflow: 'initial' }}>
+                     <div style={{
+                        padding: this.state.collapsed ? "60px 0 0 80px" : "60px 0 0 200px",
+                        background: "#fff"
+                     }}>
+                        {this.props.children}
+                     </div>
+                  </Content>
                </Layout>
             </Layout>
          </div>
@@ -75,7 +106,7 @@ class Index extends Component {
 }
 
 export default connect(
-   ({ sidebar }) => ({
-      id: sidebar.todos.id
+   ({ navigation }) => ({
+      current_page: navigation.current_page
    })
 )(Index);
