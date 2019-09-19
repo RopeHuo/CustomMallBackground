a7120b788e9db6f0c13d323b6d888f3668c12d3f
huoqingchi <123203557@qq.com>
2019-09-19 15:51:22 +0800

    添加auth字段验证身份,删除auth高阶组件改用自定义函数

--- react_project/src/components/Home/index.js
+++ react_project/src/components/Home/index.js
@@ -1,7 +1,8 @@
 import { dynamicWrapper, createRoute } from '@/utils/core';
 const routesConfig = (app) => ({
-	path: '/home',
+	path: '/Home',
 	title: '首页',
+	auth:true,
 	component: dynamicWrapper(app, [] ,() => import('./components'))
 });
 export default (app) => createRoute(app, routesConfig);
--- react_project/src/components/PurchaseSale/OrderList/index.js
+++ react_project/src/components/PurchaseSale/OrderList/index.js
@@ -2,6 +2,7 @@ import { dynamicWrapper, createRoute } from '@/utils/core';
 const routesConfig = (app) => ({
 	path: '/OrderList',
 	title: '订单列表',
+	auth:true,
 	component: dynamicWrapper(app, [], () => import('./components'))
 });
 export default (app) => createRoute(app, routesConfig);
