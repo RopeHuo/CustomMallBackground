ff9f9fb4773fe5d35b663ff41c6a9849b9eed86d
huoqingchi <123203557@qq.com>
2019-09-20 15:45:44 +0800

    添加404页面

---
+++ react_project/src/components/NotFound/NotFound/components.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react'
+
+export default class components extends Component {
+	render() {
+		return (
+			<div>
+				<h1>都没找到哦</h1>
+			</div>
+		)
+	}
+}
