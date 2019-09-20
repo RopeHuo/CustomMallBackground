dd6bb3d6b43055256979287882b1798c89920231
huoqingchi <123203557@qq.com>
2019-09-20 15:47:48 +0800

    修改index页面,根据路由显示

--- react_project/src/pages/index/Index.js
+++ react_project/src/pages/index/Index.js
@@ -15,101 +15,107 @@ class Index extends Component {
       super();
       this.state = {
          collapsed: false,
-         brnd: props.title === "Home" ? false : true
+         brnd: props.title === "/home" ? false : true
       };
 
       //拉取默认数据
-      props.dispatch({ "type": "navigation/get_head_list" });            
+      props.dispatch({ "type": "navigation/get_head_list" });
    }
 
    onCollapse = collapsed => {
       this.setState({ collapsed });
    };
-   switching_components = e =>{      
-      this.props.dispatch({ "type":"navigation/modify_current_page","n":e.key})
-      this.props.dispatch(push(e.item.props.path))
-   }
+
    render() {
       const { routerData } = this.props;
       const { childRoutes } = routerData;
+      const path_name = this.props.location.pathname;
+      console.log();
 
+      const head_navigation = () => {
+         let dom_arr = [];
+         let head_navigation_arr = this.props.head_navigation_list;
+         for (let i = 0; i < head_navigation_arr.length; i++) {
+            dom_arr.push(
+               <Menu.Item key={head_navigation_arr[i].id}>
+                  <Link to={head_navigation_arr[i].router}><span>{head_navigation_arr[i].title}</span></Link>
+               </Menu.Item>)
+         }
+         return dom_arr
+      }
       return (
          <div>
-            <Header className="header"
-               style={{
-                  width: '100vw',
-                  position: 'fixed',
-                  top: 0,
-               }}
-            >
-               <div className="logo">多多美测试版
-               <sup style={{fontSize: "12px"}}>2.00dev</sup>
-               </div>
-               <Menu
-                  theme="dark"
-                  mode="horizontal" defaultSelectedKeys={[this.props.current_page]}
-                  style={{ lineHeight: '64px' }}
-               >
-                  <Menu.Item>
-                     <Link to="/home"><span>后台首页</span></Link>
-                  </Menu.Item>
-                  <Menu.Item>
-                     <Link to="/productManagement"><span>基础数据</span></Link>
-                  </Menu.Item>
-                  <Menu.Item>
-                     <Link to="/orderList"><span>进销存管理</span></Link>
-                  </Menu.Item>
-                  <Menu.Item>
-                     <Link to="/websiteParameters"><span>系统管理</span></Link>
-                  </Menu.Item>
-               </Menu>
-            </Header>
-            <Layout>
-               {
-                     this.state.brnd
-                     ? 
-                     <Sider
+            {
+               // eslint-disable-next-line no-prototype-builtins
+               window.dva_router_pathMap.hasOwnProperty(path_name) ?
+                  <div>
+                     <Header className="header"
                         style={{
-                           overflow: 'auto',
-                           height: '100vh',
+                           width: '100vw',
                            position: 'fixed',
-                           left: 0,
-                           top: 64
+                           top: 0,
                         }}
-                        collapsible
-                        collapsed={this.state.collapsed}
-                        onCollapse={this.onCollapse}
                      >
-                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                           <SubMenu
-                              key="sub1"
-                              title={
-                                 <span>
-                                    <Icon type="user" />
-                                    <span>User</span>
-                                 </span>
-                              }
-                           >
-                              <Menu.Item key="3">Tom</Menu.Item>
-                              <Menu.Item key="4">Bill</Menu.Item>
-                              <Menu.Item key="5">Alex</Menu.Item>
-                           </SubMenu>
+                        <div className="logo">多多美测试版
+                     <sup style={{ fontSize: "12px" }}>2.00dev</sup>
+                        </div>
+                        <Menu
+                           theme="dark"
+                           mode="horizontal" defaultSelectedKeys={["1"]}
+                           style={{ lineHeight: '64px' }}
+                        >
+                           {head_navigation()}
                         </Menu>
-                     </Sider>
-                     :
-                     null
-               }
-               <Layout>
-                  <Content style={{ margin: '24px 0 0 16px', overflow: 'initial' }}>
-                     <div style={{
-                        padding: this.state.collapsed ? "60px 0 0 80px" : "60px 0 0 200px",
-                        background: "#fff"
-                     }}>
-                        <Switch>{childRoutes}</Switch>
-                     </div>
-                  </Content>
-               </Layout>
-            </Layout>
+                     </Header>
+                     <Layout>
+                        {
+                           path_name !== "/home"
+                              ?
+                              <Sider
+                                 style={{
+                                    overflow: 'auto',
+                                    height: '100vh',
+                                    position: 'fixed',
+                                    left: 0,
+                                    top: 64
+                                 }}
+                                 collapsible
+                                 collapsed={this.state.collapsed}
+                                 onCollapse={this.onCollapse}
+                              >
+                                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                                    <SubMenu
+                                       key="sub1"
+                                       title={
+                                          <span>
+                                             <Icon type="user" />
+                                             <span>User</span>
+                                          </span>
+                                       }
+                                    >
+                                       <Menu.Item key="3">Tom</Menu.Item>
+                                       <Menu.Item key="4">Bill</Menu.Item>
+                                       <Menu.Item key="5">Alex</Menu.Item>
+                                    </SubMenu>
+                                 </Menu>
+                              </Sider>
+                              :
+                              null
+                        }
+                        <Layout>
+                           <Content style={{ paddingTop: '24px', overflow: 'initial' }}>
+                              <div style={{
+                                 padding: path_name === "/home" ? "60px 0 0 96px" : "60px 0 0 216px",
+                                 background: "#fff"
+                              }}>
+                                 <Switch>{childRoutes}</Switch>
+                              </div>
+                           </Content>
+                        </Layout>
+                     </Layout>
+                  </div>
+                  : <h1>没有找到页面</h1>
+            }
          </div>
       )
    }
@@ -117,6 +123,7 @@ class Index extends Component {
 
 export default connect(
    ({ navigation }) => ({
-      current_page: navigation.current_page
+      current_page: navigation.current_page,
+      head_navigation_list: navigation.head_navigation_list
    })
 )(Index);
