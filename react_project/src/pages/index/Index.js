813fc8d2a97b9368a861744e455727cca8495e26
huoqingchi <123203557@qq.com>
2019-09-23 11:10:34 +0800

    修改index页面

--- react_project/src/pages/index/Index.js
+++ react_project/src/pages/index/Index.js
@@ -3,19 +3,19 @@ import "./style.less";
 import React, { Component } from 'react';
 
 import { connect } from 'dva';
-import { Switch, Link } from 'dva/router';
+import { Switch } from 'dva/router';
 import { push } from 'react-router-redux';
 
 import { Layout, Menu, Icon } from 'antd';
 const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Sider } = Layout;
 
 class Index extends Component {
    constructor(props) {
       super();
       this.state = {
          collapsed: false,
-         brnd: props.title === "/home" ? false : true
+      
       };
 
       //拉取默认数据
@@ -26,23 +26,69 @@ class Index extends Component {
       this.setState({ collapsed });
    };
 
+   router_jump = path =>{
+      this.props.dispatch(push(path))    
+      //请求侧边栏数据
+      this.side_edge_navigation(path)
+   };
+
+   head_navigation = () => {
+      let dom_arr = [];
+      let head_navigation_arr = this.props.head_navigation_list;
+      for (let i = 0; i < head_navigation_arr.length; i++) {
+         dom_arr.push(
+            <Menu.Item
+               key={head_navigation_arr[i].router}
+               onClick={() => { this.router_jump(head_navigation_arr[i].router) }}
+            >
+               <span>{head_navigation_arr[i].title}</span>
+            </Menu.Item>)
+      }
+      return dom_arr
+   };
+
+   side_edge_navigation = (path) => {
+      let dom_arr = [];
+      //是否是首页，并且找到返回数据中的这条路由
+      let side_edge_navigation_item = this.props.head_navigation_list
+      .filter(item => item.router === path && path !== "/home" )[0];
+      
+      if (side_edge_navigation_item){         
+         for (let i = 0; i < side_edge_navigation_item.childer.length; i++) {
+            //判断是否有下拉框
+            side_edge_navigation_item.childer[i].title 
+            ? dom_arr.push(
+               <SubMenu
+                  key={side_edge_navigation_item.childer[i].sort}
+                  title={
+                     <span>
+                        <Icon type="user" />
+                        <span>{side_edge_navigation_item.childer[i].title}</span>
+                     </span>
+                  }
+               >
+                  {
+                     side_edge_navigation_item.childer[i].childer.map(item => {
+                        return <Menu.Item key={item.id}>{item.title}</Menu.Item>
+                     })
+                  }
+               </SubMenu>)
+            : dom_arr.push(
+                     side_edge_navigation_item.childer[i].childer.map(item => {
+                        return <Menu.Item key={item.id}>{item.title}</Menu.Item>
+                     })
+               )
+
+         }
+         return dom_arr;
+      }
+      return null;
+   }
+   
    render() {
       const { routerData } = this.props;
       const { childRoutes } = routerData;
       const path_name = this.props.location.pathname;
-      console.log();
-
-      const head_navigation = () => {
-         let dom_arr = [];
-         let head_navigation_arr = this.props.head_navigation_list;
-         for (let i = 0; i < head_navigation_arr.length; i++) {
-            dom_arr.push(
-               <Menu.Item key={head_navigation_arr[i].id}>
-                  <Link to={head_navigation_arr[i].router}><span>{head_navigation_arr[i].title}</span></Link>
-               </Menu.Item>)
-         }
-         return dom_arr
-      }
       return (
          <div>
             {
@@ -61,10 +107,11 @@ class Index extends Component {
                         </div>
                         <Menu
                            theme="dark"
-                           mode="horizontal" defaultSelectedKeys={["1"]}
+                           mode="horizontal" 
+                           selectedKeys={[path_name]}
                            style={{ lineHeight: '64px' }}
                         >
-                           {head_navigation()}
+                           {this.head_navigation()}
                         </Menu>
                      </Header>
                      <Layout>
@@ -84,19 +131,7 @@ class Index extends Component {
                                  onCollapse={this.onCollapse}
                               >
                                  <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                                    <SubMenu
-                                       key="sub1"
-                                       title={
-                                          <span>
-                                             <Icon type="user" />
-                                             <span>User</span>
-                                          </span>
-                                       }
-                                    >
-                                       <Menu.Item key="3">Tom</Menu.Item>
-                                       <Menu.Item key="4">Bill</Menu.Item>
-                                       <Menu.Item key="5">Alex</Menu.Item>
-                                    </SubMenu>
+                                    {this.side_edge_navigation(path_name)}
                                  </Menu>
                               </Sider>
                               :
