7b275df0551aebceb2a7dcda08cb56add589eac5
huoqingchi <123203557@qq.com>
2019-09-23 11:11:19 +0800

    修改为routes文件夹

---
+++ react_project/src/routes/routerTable.js
@@ -0,0 +1,44 @@
+import Index from '@/pages/index/Index';
+import ErrorPage from '@/pages/errorPage/ErrorPage';
+import Login from '@/pages/login/Login';
+
+import Home from '@/components/Home/index';
+import BasicData from '@/components/BasicData/ProductManagement/index';
+import PurchaseSale from '@/components/PurchaseSale/OrderList/index';
+import SystemManagement from '@/components/SystemManagement/WebsiteParameters/index';
+
+import NotFound from '@/components/NotFound/NotFound/index';
+
+import { createRoutes } from '@/utils/core';
+
+export const routerConfig = app => [
+	{
+		path: '/error',
+		title: '错误',
+		indexRoute: '/error/404',
+		component: ErrorPage
+	},
+	{
+		path: '/login',
+		title: '登录',
+		indexRoute: '/login/sign',
+		component: Login
+	},
+	{
+		path: '/',
+		title: '首页',
+		indexRoute: '/home',
+		component: Index,
+		auth:true,
+		childRoutes: [
+			Home(app),
+			BasicData(app),
+			PurchaseSale(app),
+			SystemManagement(app),
+			NotFound()
+		]
+	}
+];
+
+export const createRoute =  app => createRoutes(app, routerConfig);
+
