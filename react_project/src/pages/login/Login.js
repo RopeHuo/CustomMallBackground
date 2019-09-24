d75a603d0c64c7883c7e87d9933d361698c396c7
Rope <scarf666@163.com>
2019-09-24 10:21:29 +0800

    添加login页面

--- react_project/src/pages/login/Login.js
+++ react_project/src/pages/login/Login.js
@@ -1,12 +1,81 @@
 /* eslint-disable no-unused-vars */
-import  React,{ Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import { Form, Layout, Button, Icon, Input, Checkbox, Spin } from 'antd';
+import './index.less';
+const { Content } = Layout;
+const FormItem = Form.Item;
 
-export default class Login extends Component {
+class Login extends Component {
+	handleSubmit = e => {
+		const { form, dispatch } = this.props;
+		e.preventDefault();
+		form.validateFields((err, values) => {
+			// 没有error,user和pass不为空||undefined
+			if (!err && values.user && values.pass) {
+				dispatch({
+					type: 'login/async_login',
+					payload: values
+				});
+			}
+		});
+	};
 	render() {
+		const isLoading = this.props.loading.effects['user/query']
+		const { loading, form } = this.props;
+		const { getFieldDecorator } = form;
 		return (
-			<div>
-				<h1>我是登录页面</h1>
-			</div>
-		)
+			<Layout className="full-layout login-page">
+				<Content>
+					<Spin tip="登录中..." spinning={!!isLoading}>
+						<Form onSubmit={this.handleSubmit} className="login-form">
+							<div className="user-img">
+								<b>DDM</b>
+								<span>Admin</span>
+							</div>
+							<FormItem>
+								{getFieldDecorator('user', {
+									rules: [{ required: true, message: '请输入您的用户名' }]
+								})(
+									<Input
+										size="large"
+										prefix={<Icon type="user" />}
+										placeholder="用户名"
+									/>
+								)}
+							</FormItem>
+							<FormItem>
+								{getFieldDecorator('pass', {
+									rules: [{ required: true, message: '请输入您的密码' }]
+								})(
+									<Input
+										size="large"
+										prefix={<Icon type="lock" />}
+										type="password"
+										placeholder="密码"
+									/>
+								)}
+							</FormItem>
+							<FormItem>
+								<Button
+									size="large"
+									type="primary"
+									htmlType="submit"
+									className="login-form-button"
+								>
+									登录
+                </Button>
+							</FormItem>
+						</Form>
+					</Spin>
+				</Content>
+			</Layout>
+		);
 	}
 }
+const WrappedRegistrationForm = Form.create()(Login);
+export default connect(
+	({login,loading }) => ({
+		loading: loading
+	})
+)(WrappedRegistrationForm);
