1a0257efbc2dd12d83c846b3d80ae18c8c75323b
Rope <scarf666@163.com>
2019-09-27 17:28:14 +0800

    测试登录接口和mock数据

--- react_project/src/models/login/effects/effects.js
+++ react_project/src/models/login/effects/effects.js
@@ -1,9 +1,11 @@
 import $request from '@/services/request';
 import config from '@/config'
+// var a = "1115c49e61749cfb3008ae6a3ba24cb7";
 export default {
 	*async_login(action, { put }) {		
-		const result = yield $request.POST(`${config.api.LAN}/login`, action.payload);
-		console.log(result.data);
-		
+		const result = yield $request.POST(`${config.api.LAN}/menu`, {
+			'title':"ziding",
+			'route':"/a/b"
+		});
 	}
 }
--- react_project/src/models/navigation/effects/effects.js
+++ react_project/src/models/navigation/effects/effects.js
@@ -1,8 +1,11 @@
 import $request from '@/services/request';
-import config from '@/config'
+import config from '@/config';
+// import $$ from 'cmn-utils'
 export default {
-	*get_head_list(action, { put }) {		
-		const { head_navigation_list } = yield $request.GET(`${config.api.LOCAL}`);		
+	*get_head_list(action, { call, put }) {		
+		const { head_navigation_list } = yield call(() => $request.GET(`${config.api.LOCAL}`))
 		yield put({ "type": "head_navigation_list", head_navigation_list})
+		// const result = yield $$.post('/user/menu');
+		// console.log(result);
 	}
 }
